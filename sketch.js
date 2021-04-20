bec89963fa8269b966b26986468b3c45e91a1e5b
C&MASKSS <78840918+SkssOfficial@users.noreply.github.com>
2021-04-20 11:24:08 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ let poses = [];
 let stage = 0;
 let stageResult = [0, 0]
 let poseOffset = 10
-let confidenceOffset = 0.7
+let confidenceOffset = 0.85
 let stopCounting = false
 
 let currentFrame = 0
